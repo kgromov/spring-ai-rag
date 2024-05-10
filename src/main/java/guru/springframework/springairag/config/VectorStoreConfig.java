eb41af3771308349e45b15d660f4f1c31f0c32f7
konstantin <konst.gromov@gmail.com>
2024-05-11 01:04:46 +0300

    Apply prompt template and search quesry to get accurate results

--- src/main/java/guru/springframework/springairag/config/VectorStoreConfig.java
+++ src/main/java/guru/springframework/springairag/config/VectorStoreConfig.java
@@ -1,5 +1,6 @@
 package guru.springframework.springairag.config;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingClient;
@@ -9,21 +10,47 @@ import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StopWatch;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Slf4j
 @Configuration
 public class VectorStoreConfig {
+
     @Bean
     SimpleVectorStore simpleVectorStore(EmbeddingClient embeddingClient, VectorStoreProperties vectorStoreProperties) {
         var store =  new SimpleVectorStore(embeddingClient);
         File vectorStoreFile = new File(vectorStoreProperties.vectorStorePath());
         if (vectorStoreFile.exists()) {
-            store.load(vectorStoreFile);
+            this.loadData(store, vectorStoreFile);
         } else {
-            log.debug("Loading documents into vector store");
+            this.saveData(vectorStoreProperties, store, vectorStoreFile);
+        }
+        return store;
+    }
+
+    private void loadData(SimpleVectorStore store, File vectorStoreFile) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("Load data from vector store");
+        try {
+            store.load(vectorStoreFile);
+        } finally {
+            stopWatch.stop();
+            var taskInfo = stopWatch.lastTaskInfo();
+            log.info("Time to {} = {} ms", taskInfo.getTaskName(), taskInfo.getTimeMillis());
+        }
+    }
+
+    @SneakyThrows
+    private void saveData(VectorStoreProperties vectorStoreProperties, SimpleVectorStore store, File vectorStoreFile) {
+        log.info("Loading documents into vector store");
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start("Save data to vector store");
+        try {
             vectorStoreProperties.documentsToLoad().forEach(document -> {
                 log.debug("Loading document: " + document.getFilename());
                 TikaDocumentReader documentReader = new TikaDocumentReader(document);
@@ -32,8 +59,12 @@ public class VectorStoreConfig {
                 List<Document> splitDocs = textSplitter.apply(docs);
                 store.add(splitDocs);
             });
+            Files.createFile(Paths.get(vectorStoreFile.getAbsolutePath()));
             store.save(vectorStoreFile);
+        } finally {
+            stopWatch.stop();
+            var taskInfo = stopWatch.lastTaskInfo();
+            log.info("Time to {} = {} ms", taskInfo.getTaskName(), taskInfo.getTimeMillis());
         }
-        return store;
     }
 }
--- src/main/java/guru/springframework/springairag/service/OpenAIService.java
+++ src/main/java/guru/springframework/springairag/service/OpenAIService.java
@@ -6,21 +6,40 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.embedding.EmbeddingClient;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.joining;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class OpenAIService {
     private final ChatClient chatClient;
-    private final EmbeddingClient embeddingClient;
     private final VectorStore vectorStore;
 
+    @Value("classpath:/prompts/rag-prompt-template-meta.st")
+    private Resource promptResource;
+
     public Answer getAnswer(Question question) {
-        var promptTemplate = new PromptTemplate(question.question());
-        var prompt = promptTemplate.create();
+        var searchRequest = SearchRequest.query(question.question()).withTopK(4);
+        List<Document> documents = vectorStore.similaritySearch(searchRequest);
+        String documentsContent = documents.stream().map(Document::getContent).collect(joining("\n"));
+
+        var promptTemplate = new PromptTemplate(promptResource);
+        var prompt = promptTemplate.create(
+                Map.of(
+                        "input", question.question(),
+                        "documents", documentsContent
+                )
+        );
         var response = chatClient.call(prompt);
         return new Answer(response.getResult().getOutput().getContent());
     }
